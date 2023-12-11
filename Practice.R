97423314346457b2be7969797914a4df752782a5
MikelMoriana <mikel.moriana@gmail.com>
2023-12-11 14:41:50 +0100

    Added plamerpenguins data

--- Practice.R
+++ Practice.R
@@ -4,3 +4,6 @@ library(usethis)
 # git_default_branch_rename() to change the name of the main branch from master to main
 # git_default_branch_configure(name = "main") sp that all main branches in the future repos are called main. Already done for my PC
 
+# Making changes for fun
+library(palmerpenguins)
+data(package = "palmerpenguins", "penguins")
